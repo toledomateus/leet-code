2d74e6fdfb09a7ca301d3c0b85b5723265cb94e8
Mateus Toledo <mateustsleao@gmail.com>
2024-07-13 14:32:27 +0100

    fix: del unused variables

--- codes/two-sum.py
+++ codes/two-sum.py
@@ -1,8 +1,7 @@
+from typing import List
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         mapValInd = {}
-        count = target
-        answer: List[int] = []
         for i,n in enumerate(nums):
             diff = target - n
             if diff in mapValInd:
